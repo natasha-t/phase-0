574d0ba67598713e443d2341413767e2b12fa816
Natasha Thapliyal <nthapli@gmail.com>
2015-11-12 10:32:42 -0800

    Update group_project_solution.js

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -1,10 +1,14 @@
 // Person 1 - Tests to User Stories
 
-// As a user, I want to use three functions to determine the sum, mean, and median of the numbers from 2 lists, one of odd length and the other of even length. The three functions are:
+// As a user, I want to use three functions to determine the sum, mean, and median of the numbers from 2 lists, 
+// one of odd length and the other of even length. The three functions are:
 // 1) sum
-    // - If I put in a list of numbers of odd length into the sum function, I will get the sum of all the values from that list. The same result will happen if I pass in a list of numbers of even length into the sum function.
+    // - If I put in a list of numbers of odd length into the sum function, I will get the sum of all the values from that list.
+    // The same result will happen if I pass in a list of numbers of even length into the sum function.
 // 2) mean
-    // - If I pass in the odd length #'s list into the mean function, I will get the average of those numbers. Same result will happen if I pass the even length #'s list.
+    // - If I pass in the odd length #'s list into the mean function, I will get the average of those numbers. 
+    // Same result will happen if I pass the even length #'s list.
 // 3) median
-    // - If I pass in the odd length #'s list into the median function, I will get the median value of those numbers. Same result will occur if I pass the even length #'s list.
+    // - If I pass in the odd length #'s list into the median function, I will get the median value of those numbers. 
+    // Same result will occur if I pass the even length #'s list.
 
